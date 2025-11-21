2acabc3d4d2d057a9d245457370a63e7db3496a0
Stepanyan-levon <slevon192@gmail.com>
2025-11-20 18:40:27 -0800

    css and js changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function readMore(){
    const bossEl=document.querySelector('.boss')
    const textEl=document.querySelector('.text')
+    const paraEl = document.querySelector('.para')
  // textEl.innerText=`It is a long established fact that a reader
  // will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`
  //
 textEl.style.color = 'red'
     bossEl.style.backgroundColor = 'green'
-
-
+        paraEl.innerText = `Classic Recreations has been building officially licensed Shelby Mustangs for a while, and according to our friends at AUTOMOBILE, they're good. Really good. Recently, CR landed the license to build three more classic Mustangs: the 1969-1970 Boss 302, the 1969-1970 Boss 429, and the 1969-1970 Mach 1. These aren't resto-mods, either. Underneath the vintage sheetmetal, you essentially get a brand new car.`
 }
